d1bd7a5590940a98d3aa019fd34c0ac1cc80ffc9
dshreddy03 <112101014@smail.iitpkd.ac.in>
2024-01-19 15:12:00 +0530

    recursion concept done

---
+++ 3_recursion/factorial.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define int long long int
+
+using namespace std;
+
+int fibonacci(int n) {
+
+    // Base case: n = 0 or 1
+    if (n <= 1) {
+        return n;
+    }
+    
+    // Recursive case: fib(n) = fib(n - 1) + fib(n - 1)
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+signed main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+
+   solve();
+   cout<<(float)clock() / CLOCKS_PER_SEC<<endl;
+   return 0;
+}
---
+++ 3_recursion/recursion.md
@@ -0,0 +1,11 @@
+# Recursion
+
+The best way to represent recursion is a decision tree & call stack.
+
+<img src="recursionTree.png">
+<img src="recursionTree2.png">
+
+## PROBLEMS
+
+- https://leetcode.com/problems/fibonacci-number/
+- https://leetcode.com/problems/climbing-stairs/
